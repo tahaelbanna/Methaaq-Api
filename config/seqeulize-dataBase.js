acfcf6e2917313f72177a62641991633f82db59d
Taha Elbanna <elbannataha@gmail.com>
2026-03-24 20:29:08 +0200

    configure sequelize for production

--- config/seqeulize-dataBase.js
+++ config/seqeulize-dataBase.js
@@ -1,15 +1,28 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-    process.env.PG_DB_NAME,     
-    process.env.PG_USER,        
-    process.env.PG_PASSWORD,   
-    {
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres',
-        host: process.env.PG_HOST,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
         logging: false,
-    }
-);
-
+    });
+} else {
+    sequelize = new Sequelize(
+        process.env.PG_DB_NAME,
+        process.env.PG_USER,
+        process.env.PG_PASSWORD,
+        {
+            dialect: 'postgres',
+            host: process.env.PG_HOST,
+            logging: false,
+        }
+    );
+}
 module.exports = sequelize;
